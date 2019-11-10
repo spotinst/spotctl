aab43720d1a25196e4891d840d780df5705e91ab
liranp <liran.polak@gmail.com>
2019-11-10 18:56:11 +0200

    fix(cmd/completion): change path in usage example

--- internal/cmd/completion/completion.go
+++ internal/cmd/completion/completion.go
@@ -146,16 +146,16 @@ Example installation instructions:
 macOS:
 	$ brew install bash-completion
 	$ source $(brew --prefix)/etc/bash_completion
-	$ mkdir -p ~/.spotinst
-	$ spotctl completion > ~/.spotinst/spotctl-completion
-	$ source ~/.spotinst/spotctl-completion
+	$ mkdir -p ~/.spotinst/spotctl
+	$ spotctl completion > ~/.spotinst/spotctl/completion
+	$ source ~/.spotinst/spotctl/completion
 
 Ubuntu:
 	$ apt-get install bash-completion
 	$ source /etc/bash-completion
-	$ mkdir -p ~/.spotinst
-	$ spotctl completion > ~/.spotinst/spotctl-completion
-	$ source ~/.spotinst/spotctl-completion
+	$ mkdir -p ~/.spotinst/spotctl
+	$ spotctl completion > ~/.spotinst/spotctl/completion
+	$ source ~/.spotinst/spotctl/completion
 
 Additionally, you may want to output the completion to a file and source in your shell rcfile.`
 
