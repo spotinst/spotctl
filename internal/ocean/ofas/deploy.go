2cb27a21ffa12eceff74c0101de6093b7a2dad63
Ragnar Valdimarsson <ragnar@netapp.com>
2023-09-21 15:24:41 +0200

    BGD-3434 cleanup

--- internal/ocean/ofas/deploy.go
+++ internal/ocean/ofas/deploy.go
@@ -6,15 +6,13 @@ import (
 	"io"
 	corev1 "k8s.io/api/core/v1"
 	rbakv1 "k8s.io/api/rbac/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes"
 	"strings"
-	"time"
-
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/spotinst/spotctl/internal/ocean/ofas/config"
 )
@@ -23,9 +21,6 @@ const (
 	spotConfigMapNamespace        = metav1.NamespaceSystem
 	spotConfigMapName             = "spotinst-kubernetes-cluster-controller-config"
 	clusterIdentifierConfigMapKey = "spotinst.cluster-identifier"
-
-	pollInterval = 5 * time.Second
-	pollTimeout  = 5 * time.Minute
 )
 
 func ValidateClusterContext(ctx context.Context, client kubernetes.Interface, clusterIdentifier string) error {
@@ -42,28 +37,12 @@ func ValidateClusterContext(ctx context.Context, client kubernetes.Interface, cl
 	return nil
 }
 
-func CreateDeployerRBAC_OLD(ctx context.Context, client kubernetes.Interface, namespace string) error {
-	sa, crb, err := config.GetDeployerRBAC(namespace)
-	if err != nil {
-		return fmt.Errorf("could not get deployer rbac objects, %w", err)
-	}
-
-	_, err = client.CoreV1().ServiceAccounts(namespace).Create(ctx, sa, metav1.CreateOptions{})
-	if err != nil && !k8serrors.IsAlreadyExists(err) {
-		return fmt.Errorf("could not create deployer service account, %w", err)
-	}
-
-	_, err = client.RbacV1().ClusterRoleBindings().Create(ctx, crb, metav1.CreateOptions{})
-	if err != nil && !k8serrors.IsAlreadyExists(err) {
-		return fmt.Errorf("could not create deployer cluster role binding, %w", err)
-	}
-
-	return nil
-}
-
 func CreateDeployerRBAC(ctx context.Context, client kubernetes.Interface) error {
-	largeYAML, _ := config.GetDeploymentYaml()
-	decoder := yaml.NewYAMLOrJSONDecoder(strings.NewReader(largeYAML), 4096)
+	deploymentYaml, err := config.GetDeploymentYaml()
+	if err != nil {
+		return fmt.Errorf("could not get deployment yaml, %w", err)
+	}
+	decoder := yaml.NewYAMLOrJSONDecoder(strings.NewReader(deploymentYaml), 4096)
 
 	for {
 		// Decode the YAML into an unstructured object
@@ -71,7 +50,7 @@ func CreateDeployerRBAC(ctx context.Context, client kubernetes.Interface) error 
 		if err := decoder.Decode(&obj); err == io.EOF {
 			break
 		} else if err != nil {
-			panic(err)
+			return fmt.Errorf("could not decode yaml, %w", err)
 		}
 
 		// Handle each resource type
@@ -121,7 +100,10 @@ func CreateDeployerRBAC(ctx context.Context, client kubernetes.Interface) error 
 			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, crb); err != nil {
 				return fmt.Errorf("could not decode cluster role binding yaml, %w", err)
 			}
-			_, _ = client.RbacV1().ClusterRoleBindings().Create(ctx, crb, metav1.CreateOptions{})
+			_, err := client.RbacV1().ClusterRoleBindings().Create(ctx, crb, metav1.CreateOptions{})
+			if err != nil && !k8serrors.IsAlreadyExists(err) {
+				return fmt.Errorf("could not create cluster role binding, %w", err)
+			}
 		}
 	}
 
