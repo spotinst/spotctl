97eccafbfc84a5dae34684b0c7980e55cf20c8d7
Ragnar Valdimarasson <110550239+raggivald@users.noreply.github.com>
2023-09-26 14:52:21 +0000

    Bgd 3434 spotctl should use restricted rbac (#567)
    * BGD-3434 User restricted-RBAC
    
    * BGD-3434 yaml file decode into unstructured obj and each resorce kind created
    
    * BGD-3434 cleanup
    
    * BGD-3434 warning if unsupported resource kind is found in deployment yaml

--- internal/ocean/ofas/config/rbac.go
+++ internal/ocean/ofas/config/rbac.go
@@ -1,105 +1,28 @@
 package config
 
 import (
-	"bytes"
 	"fmt"
-	"strings"
-	"text/template"
-
-	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	yamlutil "k8s.io/apimachinery/pkg/util/yaml"
+	"io"
+	"net/http"
 )
 
-const (
-	ServiceAccountName = "bigdata-deployer"
-	RoleBindingName    = "bigdata-deployer-admin"
-)
+const OceanSparkDeployYamlUrl = "https://spotinst-public.s3.amazonaws.com/integrations/kubernetes/ocean-spark/templates/ocean-spark-deploy.yaml"
 
-type rbacValues struct {
-	ServiceAccountName      string
-	ServiceAccountNamespace string
-	RoleBindingName         string
+func GetDeploymentYaml() (string, error) {
+	resp, err := http.Get(OceanSparkDeployYamlUrl)
+	if err != nil {
+		return "", fmt.Errorf("error fetching the ocean-spark-deploy.yaml from %s: %w", OceanSparkDeployYamlUrl, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("could not read ocean-spark-deploy.yaml from %s: unexpected status code %d: %s", OceanSparkDeployYamlUrl, resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body for ocean-spark-deploy.yaml from %s: %w", OceanSparkDeployYamlUrl, err)
+	}
+
+	return string(data), nil
 }
-
-type rbacManifests struct {
-	serviceAccount     string
-	clusterRoleBinding string
-}
-
-func GetDeployerRBAC(namespace string) (*corev1.ServiceAccount, *rbacv1.ClusterRoleBinding, error) {
-	manifests, err := getRBACManifests(namespace)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not get rbac manifests, %w", err)
-	}
-
-	sa := &corev1.ServiceAccount{}
-	err = yamlutil.NewYAMLOrJSONDecoder(strings.NewReader(manifests.serviceAccount), len(manifests.serviceAccount)).Decode(sa)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not decode service account yaml, %w", err)
-	}
-
-	crb := &rbacv1.ClusterRoleBinding{}
-	err = yamlutil.NewYAMLOrJSONDecoder(strings.NewReader(manifests.clusterRoleBinding), len(manifests.clusterRoleBinding)).Decode(crb)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not decode cluster role binding yaml, %w", err)
-	}
-
-	return sa, crb, nil
-}
-
-func getRBACManifests(namespace string) (*rbacManifests, error) {
-	values := rbacValues{
-		ServiceAccountName:      ServiceAccountName,
-		ServiceAccountNamespace: namespace,
-		RoleBindingName:         RoleBindingName,
-	}
-
-	saTemplate, err := template.New("sa").Parse(serviceAccountTemplate)
-	if err != nil {
-		return nil, fmt.Errorf("could not parse service account template, %w", err)
-	}
-
-	saManifest := new(bytes.Buffer)
-	err = saTemplate.Execute(saManifest, values)
-	if err != nil {
-		return nil, fmt.Errorf("could not execute service account template, %w", err)
-	}
-
-	rbTemplate, err := template.New("roleBinding").Parse(roleBindingTemplate)
-	if err != nil {
-		return nil, fmt.Errorf("could not parse role binding template, %w", err)
-	}
-
-	rbManifest := new(bytes.Buffer)
-	err = rbTemplate.Execute(rbManifest, values)
-	if err != nil {
-		return nil, fmt.Errorf("could not execute role binding template, %w", err)
-	}
-
-	return &rbacManifests{
-		serviceAccount:     saManifest.String(),
-		clusterRoleBinding: rbManifest.String(),
-	}, nil
-}
-
-const serviceAccountTemplate = `apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: {{.ServiceAccountName}}
-  namespace: {{.ServiceAccountNamespace}}
-`
-
-const roleBindingTemplate = `apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: {{.RoleBindingName}}
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-  - kind: ServiceAccount
-    name: {{.ServiceAccountName}}
-    namespace: {{.ServiceAccountNamespace}}
-`
--- internal/ocean/ofas/deploy.go
+++ internal/ocean/ofas/deploy.go
@@ -3,12 +3,19 @@ package ofas
 import (
 	"context"
 	"fmt"
-	"time"
+	"io"
+	"strings"
 
+	corev1 "k8s.io/api/core/v1"
+	rbakv1 "k8s.io/api/rbac/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes"
 
+	"github.com/spotinst/spotctl/internal/log"
 	"github.com/spotinst/spotctl/internal/ocean/ofas/config"
 )
 
@@ -16,9 +23,6 @@ const (
 	spotConfigMapNamespace        = metav1.NamespaceSystem
 	spotConfigMapName             = "spotinst-kubernetes-cluster-controller-config"
 	clusterIdentifierConfigMapKey = "spotinst.cluster-identifier"
-
-	pollInterval = 5 * time.Second
-	pollTimeout  = 5 * time.Minute
 )
 
 func ValidateClusterContext(ctx context.Context, client kubernetes.Interface, clusterIdentifier string) error {
@@ -35,20 +39,77 @@ func ValidateClusterContext(ctx context.Context, client kubernetes.Interface, cl
 	return nil
 }
 
-func CreateDeployerRBAC(ctx context.Context, client kubernetes.Interface, namespace string) error {
-	sa, crb, err := config.GetDeployerRBAC(namespace)
+func CreateDeployerRBAC(ctx context.Context, client kubernetes.Interface) error {
+	deploymentYaml, err := config.GetDeploymentYaml()
 	if err != nil {
-		return fmt.Errorf("could not get deployer rbac objects, %w", err)
+		return fmt.Errorf("could not get deployment yaml, %w", err)
 	}
+	decoder := yaml.NewYAMLOrJSONDecoder(strings.NewReader(deploymentYaml), 4096)
 
-	_, err = client.CoreV1().ServiceAccounts(namespace).Create(ctx, sa, metav1.CreateOptions{})
-	if err != nil && !k8serrors.IsAlreadyExists(err) {
-		return fmt.Errorf("could not create deployer service account, %w", err)
-	}
+	for {
+		// Decode the YAML into an unstructured object
+		var obj unstructured.Unstructured
+		if err := decoder.Decode(&obj); err == io.EOF {
+			break
+		} else if err != nil {
+			return fmt.Errorf("could not decode yaml, %w", err)
+		}
 
-	_, err = client.RbacV1().ClusterRoleBindings().Create(ctx, crb, metav1.CreateOptions{})
-	if err != nil && !k8serrors.IsAlreadyExists(err) {
-		return fmt.Errorf("could not create deployer cluster role binding, %w", err)
+		// Handle each resource type
+		switch obj.GetKind() {
+		case "Namespace":
+			ns := &corev1.Namespace{}
+			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, ns); err != nil {
+				return fmt.Errorf("could not decode namespace yaml, %w", err)
+			}
+			_, err := client.CoreV1().Namespaces().Create(ctx, ns, metav1.CreateOptions{})
+			if err != nil && !k8serrors.IsAlreadyExists(err) {
+				return fmt.Errorf("could not create namespace, %w", err)
+			}
+
+		case "ServiceAccount":
+			sa := &corev1.ServiceAccount{}
+			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, sa); err != nil {
+				return fmt.Errorf("could not decode service account yaml, %w", err)
+			}
+			_, err := client.CoreV1().ServiceAccounts(sa.Namespace).Create(ctx, sa, metav1.CreateOptions{})
+			if err != nil && !k8serrors.IsAlreadyExists(err) {
+				return fmt.Errorf("could not create service account, %w", err)
+			}
+
+		case "RoleBinding":
+			rb := &rbakv1.RoleBinding{}
+			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, rb); err != nil {
+				return fmt.Errorf("could not decode role binding yaml, %w", err)
+			}
+			_, err := client.RbacV1().RoleBindings(rb.Namespace).Create(ctx, rb, metav1.CreateOptions{})
+			if err != nil && !k8serrors.IsAlreadyExists(err) {
+				return fmt.Errorf("could not create role binding, %w", err)
+			}
+
+		case "ClusterRole":
+			cr := &rbakv1.ClusterRole{}
+			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, cr); err != nil {
+				return fmt.Errorf("could not decode cluster role yaml, %w", err)
+			}
+			_, err := client.RbacV1().ClusterRoles().Create(ctx, cr, metav1.CreateOptions{})
+			if err != nil && !k8serrors.IsAlreadyExists(err) {
+				return fmt.Errorf("could not create cluster role, %w", err)
+			}
+
+		case "ClusterRoleBinding":
+			crb := &rbakv1.ClusterRoleBinding{}
+			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, crb); err != nil {
+				return fmt.Errorf("could not decode cluster role binding yaml, %w", err)
+			}
+			_, err := client.RbacV1().ClusterRoleBindings().Create(ctx, crb, metav1.CreateOptions{})
+			if err != nil && !k8serrors.IsAlreadyExists(err) {
+				return fmt.Errorf("could not create cluster role binding, %w", err)
+			}
+
+		default:
+			log.Warnf("Skipping unsupported resource type %q", obj.GetKind())
+		}
 	}
 
 	return nil
